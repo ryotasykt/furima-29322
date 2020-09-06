c3c3aa8f0c26b205b82204d2d94a348a751c2376
ryotasykt <ryotasykt@gmail.com>
2020-09-06 21:34:49 +0900

    userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,11 @@ class User < ApplicationRecord
          with_options presence: true do
            validates :nickname, length: { maximum: 40 }
            validates :email, uniqueness: true
-           validates :password, length: {minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-           validates :password_confirmation, length: {minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-           validates :family_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-           validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-           validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-           validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+           validates :password, length: {minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+           validates :family_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+           validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+           validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'Full-width katakana characters' }
+           validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'Full-width katakana characters'}
            validates :birth_day
          end
 end
