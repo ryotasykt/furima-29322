439719d9cde64ef8c33fe9e026d5c40f7f0d1165
ryotasykt <ryotasykt@gmail.com>
2020-09-07 14:50:04 +0900

    ユーザー管理機能の実装（修正1）

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, length: { maximum: 40 }
     validates :email, uniqueness: true
-    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
     validates :family_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
     validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
