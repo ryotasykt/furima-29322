71f3ea7aba91e81027e021e14a00bafc21b8de65
ryotasykt <ryotasykt@gmail.com>
2020-09-10 14:44:26 +0900

    画面遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,16 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :days_until_shipping_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_sign_in
