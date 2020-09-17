a5cb25fe5a518d4fb33c913020b5158cd5841c66
ryotasykt <ryotasykt@gmail.com>
2020-09-17 12:39:50 +0900

    商品購入画面の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :redirect_root, only: [:index]
 
   def index
   end
 
   def create
+  end  
+
+  private
+
+  def redirect_root
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
-  
+
 end
