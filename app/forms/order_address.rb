8b3064bfb92fbd5100d29698b517abf8ac807d86
ryotasykt <ryotasykt@gmail.com>
2020-09-20 23:10:37 +0900

    バリデーションの設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,6 +2,20 @@ class OrderAddress
 
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :token, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+  
+  POSTEL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  PHONE_NUMBER_REGEX = /\A\d{10,11}\z/.freeze
+
+  with_options presence: true do
+    # 「クレジットカード情報」に関するバリデーション
+    validates :token
+    # 「配送先住所」に関するバリデーション
+    validates :postal_code, format: { with: POSTEL_CODE_REGEX }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX }
+  end
 
   def save
     # 「購入」の情報を保存し、「order」という変数に入れている
