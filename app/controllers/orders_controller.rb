4c229c07c874e368ca45385195d744aa3792ecd1
ryotasykt <ryotasykt@gmail.com>
2020-09-20 22:23:31 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,13 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order = OrderAddress.new(order_params)  #「OrderAddress」に編集
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end  
 
   private
@@ -23,6 +30,15 @@ class OrdersController < ApplicationController
   def order_params
     #「配送先住所」のキーも追加
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_56e59faec727342a2b15064a"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,           # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 
 end
